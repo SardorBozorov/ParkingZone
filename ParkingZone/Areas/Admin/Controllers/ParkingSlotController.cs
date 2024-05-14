bdec748257371eda8d9022e9ea8be721696c866f
SardorBozorov <bozorovsardor0610@gmail.com>
2024-05-14 19:45:23 +0500

    Create Slote action  added

--- ParkingZone/Areas/Admin/Controllers/ParkingSlotController.cs
+++ ParkingZone/Areas/Admin/Controllers/ParkingSlotController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Parking_Zone.MVC.Models.ParkingSlotVMs;
 using Parking_Zone.Service.Interfaces;
+using System.Security.Policy;
 
 namespace Parking_Zone.MVC.Areas.Admin.Controllers;
 
@@ -10,15 +11,46 @@ namespace Parking_Zone.MVC.Areas.Admin.Controllers;
 public class ParkingSlotController : Controller
 {
     private readonly IParkingSlotService _parkingSlotService;
-    public ParkingSlotController(IParkingSlotService parkingSlotService)
+    private readonly IParkingZoneService _parkingZoneService;
+    public ParkingSlotController(IParkingSlotService parkingSlotService, IParkingZoneService parkinZoneService)
     {
         _parkingSlotService = parkingSlotService;
+        _parkingZoneService = parkinZoneService;
     }
 
-    public IActionResult Index(long parkingZoneId)
+    public IActionResult Index(long zoneId)
     {
-        var parkingSlots = _parkingSlotService.GetSlotsByZoneId(parkingZoneId);
-        var listItemVMs = ListOfSlotsVM.MapToVM(parkingSlots);
+        //var parkingSlots = _parkingSlotService.GetAll();
+        //var listItemsVMs = ListOfSlotsVM.MapToVM(parkingSlots);
+
+        //ViewData["parkingZoneId"] = parkingZoneId;
+        //return View(listItemsVMs);
+        var parkingSlots = _parkingSlotService.GetSlotsByZoneId(zoneId);
+        var listItemVMs = ListOfSlotsVM.MapToVM(parkingSlots).ToList();
+        var zone = _parkingZoneService.GetById(zoneId);
+        ViewData["parkingZoneId"] = zone.Id;
+        ViewData["name"] = zone.Name;
         return View(listItemVMs);
+    }
+
+    public IActionResult Create(long parkingZoneId)
+    {
+        CreateVM createVM = new()
+        {
+            ParkingZoneId = parkingZoneId,
+        };
+        return View(createVM);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(CreateVM createVM)
+    {
+        if (ModelState.IsValid)
+        {
+            _parkingSlotService.Create(createVM.MapToModel());
+            return RedirectToAction(nameof(Index));
+        }
+        return View(createVM);
     }
 }
